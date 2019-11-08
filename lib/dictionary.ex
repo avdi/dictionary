c63d94300e3cfea2765583d248401d330f88e8b1
Jessica Kerr <jessitron@atomist.com>
2019-11-08 19:01:23 +0000

    Configure bulid task

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -5,7 +5,7 @@ defmodule Dictionary do
 
   def word_list do
     contents = File.read!("assets/words.txt")
-    list     = String.split(contents, ~r/\n/)
+    list = String.split(contents, ~r/\n/)
     list
   end
 end
